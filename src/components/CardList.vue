2624b731189130c24380908ac7e2fe6e2161ac77
Farid Aliev <farid-keltuzed@mail.ru>
2025-03-08 23:19:31 +0300

    Card added

---
+++ src/components/CardList.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Card from './Card.vue'
+
+const onClickAdd = () => {
+    alert('Добавлено!');
+}
+</script>
+
+<template>
+    <div class="grid grid-cols-4 gap-5 ">
+     <Card
+     :price="12999"
+     title="Мужские кроссовки Nike Blazer Mid Suede"
+     image-url="/sneakers/sneakers-1.jpg"
+     :is-favorite="false"
+     :onClickAdd="onClickAdd"
+     :is-added="true"
+     
+     
+     />
+
+   </div>
+</template>
