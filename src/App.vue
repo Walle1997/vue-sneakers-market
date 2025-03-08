56c581a6f4d005058b777638de04bbde24bc0239
Farid Aliev <farid-keltuzed@mail.ru>
2025-03-08 21:51:53 +0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+ import Header from './components/Header.vue';
+
+</script>
+
+<template>
+<div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
+  <Header />
+  <div class="">
+    <div>
+      <img src="/like-2.svg" alt="Like 1" />
+      <img src="/sneakers/sneakers-1.jpg" alt="Sneaker" />
+      <p>Мужские кроссовки Nike Blazer Mid Suede</p>
+    </div>
+
+  </div>
+</div>
+</template>
+
+
+
